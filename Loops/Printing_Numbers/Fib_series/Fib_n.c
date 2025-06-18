00ab409cc05407026aea3237eae1fe5e21b265ef
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-06-18 13:41:12 +0530

    Update Fib_n.c

--- Loops/Printing_Numbers/Fib_series/Fib_n.c
+++ Loops/Printing_Numbers/Fib_series/Fib_n.c
@@ -6,11 +6,13 @@ int main()
     int n,fib = 0,i = -1,j = 1;
     printf("Enter number of Fibonacci terms:");
     scanf("%d",&n);
-    printf("The first %d numbers of fibonacci sequence:\n",n);
+    if(n<0){
+        printf("Invalid Negative Number\n");
+    }
     for(int k=1;k<=n;k++){
         fib =i+j;
-        printf("%d\n",fib);
-        i = j , j = fib;
+        printf("Fibonacci Term %d : %d\n",k,fib);
+        i = j,j = fib;
     }
     return 0;
 }
