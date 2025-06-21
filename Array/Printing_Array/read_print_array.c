1970bc2acebc654f02901d003fd4921ccde49ec0
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-06-21 19:50:25 +0530

    Create read_print_array.c

---
+++ Array/Printing_Array/read_print_array.c
@@ -0,0 +1,27 @@
+// Reading and printing elements of an array
+
+#include <stdio.h>
+int main()
+{
+   int i,j,n,num[100];
+   while(1)
+    {
+      printf("Enter number of elements:");
+      scanf("%d",&n);
+      if(n>100||n<=0){
+           printf("Invalid input ! Enter number of elements between 1 and 100\n\n");
+           continue;
+    }
+    else{
+        for(i=0;i<n;i++){                         //loop for storing elements in an array
+        printf("Enter element %d:",i+1);
+        scanf("%d",&num[i]);
+   }
+        printf("\nElements of an array:\n");   //loop for printing elements of array
+        for(j=0;j<n;j++){
+        printf("%d\t",num[j]);
+         }
+        }
+        break;                              //Exiting after valid input
+   }
+}
