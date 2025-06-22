cc0af1ba4c9a5e303f6cba82a01dea3853d83843
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-06-22 18:55:49 +0530

    Update odd_even_array.c

--- Array/Printing_Array/odd_even_array.c
+++ Array/Printing_Array/odd_even_array.c
@@ -1,24 +1,26 @@
+//Printing total number of even and odd elements in an array
+
 #include<stdio.h>
 int main()
 {
     int odd_count=0,even_count=0;
     int num[100],n;
-     
+
      while(1)
      {
          printf("Enter number of elements between 1 and 100:");
          scanf("%d",&n);
-    
+
     if(n>100 || n<=0){
         printf("Invalid Input ! Enter a valid number between 1 and 100\n\n");
         continue;
     }
-    
+
     for(int i=0;i<n;i++)
     {
         printf("Enter element %d:",i+1);
         scanf("%d",&num[i]);
-        
+
         if(num[i]%2==1){
             odd_count++;
         }
@@ -26,8 +28,9 @@ int main()
             even_count++;
         }
      }
-       break; 
+       break;
     }
     printf("Even elements in an array:%d\n",even_count);
-    printf("Odd elements in an array:%d\n",odd_count); 
+    printf("Odd elements in an array:%d\n",odd_count);
+    return 0;
 }
